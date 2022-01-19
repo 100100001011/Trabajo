749a46f455401356c50fa0c3ce47b725825e0221
CrisAlejo23 <89047686+CrisAlejo23@users.noreply.github.com>
2022-01-19 12:56:31 -0500

    Update conexion.php

--- Prueba/Model/conexion.php
+++ Prueba/Model/conexion.php
@@ -3,8 +3,8 @@
 $usuario= "root";
 $clave= "";
 $server= "localhost";
-$bdd = "prueba";
-//$bdd = "pedidos";
+//$bdd = "prueba";
+$bdd = "trabajo";
 
 $cnx = new mysqli($server,$usuario,$clave,$bdd);
 
